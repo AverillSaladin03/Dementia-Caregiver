11559ec7cf98855af2349310d66e439c48f97406
AverillSaladin03 <averillsaladin2003@gmail.com>
2023-06-20 08:25:54 +0700

    Commit test by adding Navigation Title

--- Dementia Caregiver/ContentView.swift
+++ Dementia Caregiver/ContentView.swift
@@ -28,6 +28,7 @@ struct ContentView: View {
                 }
                 .onDelete(perform: deleteItems)
             }
+            .navigationTitle("Tes Commit")
             .toolbar {
                 ToolbarItem(placement: .navigationBarTrailing) {
                     EditButton()
