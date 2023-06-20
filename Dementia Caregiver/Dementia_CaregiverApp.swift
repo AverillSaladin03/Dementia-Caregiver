21e233906018b6ab3f35e351a719b2ffbd0f6371
AverillSaladin03 <averillsaladin2003@gmail.com>
2023-06-20 08:22:06 +0700

    Initial Commit

---
+++ Dementia Caregiver/Dementia_CaregiverApp.swift
@@ -0,0 +1,20 @@
+//
+//  Dementia_CaregiverApp.swift
+//  Dementia Caregiver
+//
+//  Created by Averill Saladin Atma Setiawan on 20/06/23.
+//
+
+import SwiftUI
+
+@main
+struct Dementia_CaregiverApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
