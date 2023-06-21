1a327ecc8e43fa5008823b541a3cac4d65bdb850
Reyner Fernaldi <reynerfernaldi@gmail.com>
2023-06-21 09:55:36 +0700

    Add .gitignore file

---
+++ Dementia Caregiver/View/DummyView.swift
@@ -0,0 +1,20 @@
+//
+//  DummyView.swift
+//  Dementia Caregiver
+//
+//  Created by Reyner Fernaldi on 23/06/23.
+//
+
+import SwiftUI
+
+struct DummyView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DummyView_Previews: PreviewProvider {
+    static var previews: some View {
+        DummyView()
+    }
+}
