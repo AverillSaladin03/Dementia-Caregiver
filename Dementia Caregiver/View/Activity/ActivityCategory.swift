a5d483056de38864a3fb01378742ff82f9eba7f0
ZefanyaOkta <75228935+ZefanyaOkta@users.noreply.github.com>
2023-06-28 09:34:57 +0700

    Activity

--- Dementia Caregiver/View/Activity/ActivityCategory.swift
+++ Dementia Caregiver/View/Activity/ActivityCategory.swift
@@ -8,41 +8,58 @@
 import SwiftUI
 
 struct ActivityCategory: View {
-    let categoryImage = ["contoh3", "contoh3", "contoh3", "contoh3", "contoh3", "contoh3", "contoh3", "contoh3"]
+    private var categoryImage = ["contoh3", "contoh", "contoh2", "contoh2", "contoh3", "contoh3", "contoh3", "contoh"]
+//    private var gridItemLayout = [GridItem(.flexible()), GridItem(.flexible())]
     @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
     
     var body: some View {
-        VStack {
-            ForEach(0..<categoryImage.count / 2, id: \.self) { rowIndex in
-                HStack {
-                    ForEach(0..<2, id: \.self) { columnIndex in
-                        let index = rowIndex * 2 + columnIndex
-                        NavigationLink(destination: ActivityList()) {
-                            if index < categoryImage.count {
-                                Image(categoryImage[index])
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fit)
-                                    .frame(width: 160, height: 160)
-                                    .cornerRadius(10)
-                            } else {
-                                Spacer()
+//        ScrollView {
+//            LazyVGrid(columns: gridItemLayout, spacing: 10) {
+//                ForEach(categoryImage, id: \.self) { categoryImage in
+//                    Image(categoryImage)
+//                        .resizable()
+//                        .aspectRatio(contentMode: .fill)
+//                        .frame(width: 160, height: 160) // Adjust the height as needed
+//                        .cornerRadius(10)
+//                }
+//            }
+//            .padding()
+//        }
+        
+        ScrollView{
+            VStack {
+                ForEach(0..<categoryImage.count / 2, id: \.self) { rowIndex in
+                    HStack {
+                        ForEach(0..<2, id: \.self) { columnIndex in
+                            let index = rowIndex * 2 + columnIndex
+                            NavigationLink(destination: ActivityList()) {
+                                if index < categoryImage.count {
+                                    Image(categoryImage[index])
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fill)
+                                        .frame(width: 160, height: 160)
+                                        .cornerRadius(10)
+                                } else {
+                                    Spacer()
+                                }
                             }
+                            .padding(8)
                         }
-                        .padding(8)
                     }
                 }
             }
-            .navigationBarTitle("Kategori Aktivitas", displayMode: .inline)
-            .navigationBarBackButtonHidden(true)
-            .navigationBarItems(leading:
-                                    Button(action: {
-                presentationMode.wrappedValue.dismiss()
-            }) {
-                Image(systemName: "chevron.left")
-                Text("Aktivitas")
-            }
-            )
+        
+        .navigationBarTitle("Kategori Aktivitas", displayMode: .inline)
+        .navigationBarBackButtonHidden(true)
+        .navigationBarItems(leading:
+                                Button(action: {
+            presentationMode.wrappedValue.dismiss()
+        }) {
+            Image(systemName: "chevron.left")
+            Text("Aktivitas")
         }
+        )
+                }
     }
 }
 
